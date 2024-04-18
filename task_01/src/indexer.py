a7ff05708c357b18295ccc16a7476bfe62993b40
Victor Ribeiro <ribeir145@gmail.com>
2024-04-17 22:57:21 -0300

    Fixed vector model to include idf

--- task_01/src/indexer.py
+++ task_01/src/indexer.py
@@ -3,8 +3,9 @@ import ast
 import re
 import math
 import pandas as pd
+import numpy as np
 
-SRC_FOLDER_PATH = ''
+SRC_FOLDER_PATH = r'D:\UFRJ\9º Período\Busca e Mineração de Texto\cos738-search-and-text-mining\task_01\src\\'
 
 class Indexer:
     
@@ -45,12 +46,11 @@ class Indexer:
         
         return inv_list
     
-    def write_output_file(self, term_doc_matrix):
+    def write_output_file(self, term_doc_matrix: pd.DataFrame):
         filepath = self.result_folder_path + self.config['write']
-        with open(filepath, 'w') as f:
-            json.dump(term_doc_matrix ,f)
+        term_doc_matrix.to_csv(filepath, sep=';')
 
-    def create_term_doc_matrix(self, inv_list: dict):
+    def create_term_doc_matrix(self, inv_list: dict) -> pd.DataFrame:
         
         regex_pattern = re.compile(r'^[A-Z]{2,}$') #Over 2 characters and only letters
         filtered_inv_list = {}
@@ -59,33 +59,30 @@ class Indexer:
                 filtered_inv_list[key] = value
         
         terms = filtered_inv_list.keys()
-        docs_number = list(set().union(*inv_list.values()))
+        docs_numbers = list(set().union(*inv_list.values()))
 
-        term_x_doc_df = pd.DataFrame(index=terms, columns=docs_number, dtype='float')
+        term_x_doc_df = pd.DataFrame(index=terms, columns=docs_numbers, dtype='float')
         term_x_doc_df.fillna(0, inplace=True)
         
         for term, appearence in filtered_inv_list.items():
             for doc_number in appearence:
-                term_x_doc_df.at[term, doc_number] += 1 # Counts the term frequency in documents
+                term_x_doc_df.at[term, doc_number] += 1 #Counts the term frequency (tf) in documents
         
-        idf = {}
+        documents_count = len(docs_numbers)
+
+        term_x_doc_df['doc_freq'] = term_x_doc_df.apply(lambda row: (row != 0).sum(), axis=1) #Doc frequency for each term
+        term_x_doc_df['idf'] = np.log10(documents_count/term_x_doc_df['doc_freq'])
+        term_x_doc_df.drop('doc_freq', axis=1, inplace=True)
+
+        docs_columns = term_x_doc_df.columns[term_x_doc_df.columns != 'idf']  #Exclude 'idf' column
+        term_x_doc_df[docs_columns] = term_x_doc_df[docs_columns].mul(term_x_doc_df['idf'], axis=0) #Calculates tf-idf
 
         return term_x_doc_df
-
-        # idf = {}
-        # for term in term_doc_matrix.keys():
-        #     idf[term] = math.log(documents_count/len(term_doc_matrix[term]))
-        
-        # for term, appearence in term_doc_matrix.items():
-        #     for doc_number in appearence:
-        #         term_doc_matrix[term][doc_number] *= idf[term] # Calculates tf-idf
-        
-        # return term_doc_matrix
 
     def run(self):
         inv_list = self.read_input_file()
         term_doc_matrix = self.create_term_doc_matrix(inv_list)
-        # self.write_output_file(term_doc_matrix)
+        self.write_output_file(term_doc_matrix)
 
 
 idx = Indexer()
